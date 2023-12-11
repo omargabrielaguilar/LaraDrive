6d4f4a0476bb92b56d717e694bb77c867c40a7ad
Omar <saucyfunnel@gmail.com>
2023-12-11 17:12:06 -0500

    modal

---
+++ resources/js/Components/app/CreateFolderModal.vue
@@ -0,0 +1,13 @@
+<template>
+  <modal :show="modelValue"> Crear Modal </modal>
+</template>
+
+<script setup>
+import Modal from "@/Components/Modal.vue";
+const { modelValue } = defineProps({
+  modelValue: Boolean,
+});
+</script>
+
+<style>
+</style>
