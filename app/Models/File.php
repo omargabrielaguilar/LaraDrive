6cbdc3324d76c75fb2e30013f5e2dcabdef92012
Omar <saucyfunnel@gmail.com>
2023-12-11 17:58:35 -0500

    modal

--- app/Models/File.php
+++ app/Models/File.php
@@ -12,8 +12,35 @@ class File extends Model
 {
     use HasFactory, NodeTrait, SoftDeletes, HasCreatorAndUpdater;
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(File::class, 'parent_id');
+    }
+
     public function isOwnedBy($userId): bool
     {
         return $this->created_by == $userId;
+    }
+
+
+    public function isRoot()
+    {
+        
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if(!model->parent){
+                return;
+            }
+            $model->path = ($model->parent->)
     }
 }
--- resources/js/Components/app/CreateFolderModal.vue
+++ resources/js/Components/app/CreateFolderModal.vue
@@ -1,13 +1,89 @@
 <template>
-  <modal :show="modelValue"> Crear Modal </modal>
+    <modal :show="modelValue" @show="onShow" max-width="sm">
+        <div class="p-6">
+            <h2 class="text-lg font-medium text-gray-900">
+                Create New Folder
+            </h2>
+            <div class="mt-6">
+                <InputLabel for="folderName" value="Folder Name" class="sr-only"/>
+
+                <TextInput type="text"
+                           ref="folderNameInput"
+                           id="folderName" v-model="form.name"
+                           class="mt-1 block w-full"
+                           :class="form.errors.name ? 'border-red-500 focus:border-red-500 focus:ring-red-500' : ''"
+                           placeholder="Folder Name"
+                           @keyup.enter="createFolder"
+                />
+                <InputError :message="form.errors.name" class="mt-2"/>
+
+            </div>
+            <div class="mt-6 flex justify-end">
+                <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
+                <PrimaryButton class="ml-3"
+                               :class="{ 'opacity-25': form.processing }"
+                               @click="createFolder" :disable="form.processing">
+                    Submit
+                </PrimaryButton>
+            </div>
+        </div>
+    </modal>
 </template>
 
 <script setup>
+// Imports
 import Modal from "@/Components/Modal.vue";
-const { modelValue } = defineProps({
-  modelValue: Boolean,
-});
+import TextInput from "@/Components/TextInput.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import {useForm, usePage} from "@inertiajs/vue3";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import {nextTick, ref} from "vue";
+
+// Uses
+const form = useForm({
+    name: '',
+    parent_id: null
+})
+const page = usePage();
+
+// Refs
+const folderNameInput = ref(null)
+
+// Props & Emit
+const {modelValue} = defineProps({
+    modelValue: Boolean
+})
+const emit = defineEmits(['update:modelValue'])
+
+// Computed
+
+// Methods
+function onShow() {
+    nextTick(() => folderNameInput.value.focus())
+}
+
+function createFolder() {
+    form.post(route('folder.create'), {
+        preserveScroll: true,
+        onSuccess: () => {
+            closeModal()
+            form.reset();
+        },
+        onError: () => folderNameInput.value.focus()
+    })
+}
+
+function closeModal() {
+    emit('update:modelValue')
+    form.clearErrors();
+    form.reset()
+}
+
+// Hooks
 </script>
 
-<style>
+<style scoped>
+
 </style>
