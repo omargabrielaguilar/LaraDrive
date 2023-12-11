4dc7341363ae292f77a0d91110b481cb208fd052
Omar <saucyfunnel@gmail.com>
2023-12-11 10:52:34 -0500

    comenzamos con el controlador

--- resources/js/Components/app/UserSettingsDropdown.vue
+++ resources/js/Components/app/UserSettingsDropdown.vue
@@ -12,7 +12,7 @@
       enter-to-class="transform scale-100 opacity-100" leave-active-class="transition duration-75 ease-in"
       leave-from-class="transform scale-100 opacity-100" leave-to-class="transform scale-95 opacity-0">
       <MenuItems
-        class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+        class="absolute right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
         <div class="px-1 py-1">
           <MenuItem v-slot="{ active }">
             <ResponsiveNavLink :href="route('profile.edit')"
