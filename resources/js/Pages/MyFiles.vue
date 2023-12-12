08dd6618d96d9dd375839fa8d9a68ef86dbe2abf
Omar <saucyfunnel@gmail.com>
2023-12-12 10:13:06 -0500

    controller

--- resources/js/Pages/MyFiles.vue
+++ resources/js/Pages/MyFiles.vue
@@ -1,9 +1,48 @@
 <template>
     <AuthenticatedLayout>
-        {{ $page.url }}
+        <table class="min-w-full">
+            <thead class="bg-gray-100 border-b">
+                <tr>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Nombre
+                    </th>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Creador
+                    </th>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Ultima Modificación
+                    </th>
+                    <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                        Tamaño
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="file of files.data" :key="file.id" class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {{file.name}}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {{file.name}}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {{file.updated_at}}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {{file.size}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </AuthenticatedLayout>
 </template>
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+
+// Props & Emit
+const {files} = defineProps({
+    files: Object
+})
 </script>
